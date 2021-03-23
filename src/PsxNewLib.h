e80a83061fc898700a3a9389277347f6ab1e463c
SukkoPera <software@sukkology.net>
2021-03-23 08:36:26 +0100

    Update PsxController class for latest restructuring

--- src/PsxNewLib.h
+++ src/PsxNewLib.h
@@ -29,235 +29,10 @@
 #ifndef PSXNEWLIB_H_
 #define PSXNEWLIB_H_
 
-/** \brief Command timeout (ms)
- * 
- * Commands are sent to the controller repeatedly, until they succeed or time
- * out. This is the length of that timeout.
- * 
- * \sa COMMAND_RETRY_INTERVAL
- */
-const unsigned long COMMAND_TIMEOUT = 250;
-
-/** \brief Command Retry Interval (ms)
- * 
- * When sending a command to the controller, if it does not succeed, it is
- * retried after this amount of time.
- */
-const unsigned long COMMAND_RETRY_INTERVAL = 10;
-
-/** \brief Mode switch delay (ms)
- * 
- * After a command has been issued successfully to the controller, this amount
- * of time is waited to allow it to complete any internal procedures required to
- * execute the command.
- * 
- * \todo This is probably unnecessary.
- */
-const unsigned long MODE_SWITCH_DELAY = 500;
-
-
-/** \brief Type that is used to represent a single button in most places
- */
-enum PsxButton {
-	PSB_NONE       = 0x0000,
-	PSB_SELECT     = 0x0001,
-	PSB_L3         = 0x0002,
-	PSB_R3         = 0x0004,
-	PSB_START      = 0x0008,
-	PSB_PAD_UP     = 0x0010,
-	PSB_PAD_RIGHT  = 0x0020,
-	PSB_PAD_DOWN   = 0x0040,
-	PSB_PAD_LEFT   = 0x0080,
-	PSB_L2         = 0x0100,
-	PSB_R2         = 0x0200,
-	PSB_L1         = 0x0400,
-	PSB_R1         = 0x0800,
-	PSB_TRIANGLE   = 0x1000,
-	PSB_CIRCLE     = 0x2000,
-	PSB_CROSS      = 0x4000,
-	PSB_SQUARE     = 0x8000
-};
-
-/** \brief Type that is used to represent a single button when retrieving
- *         analog pressure data
- *
- * \sa getAnalogButton()
- */
-enum PsxAnalogButton {
-	PSAB_PAD_RIGHT  = 0,
-	PSAB_PAD_LEFT   = 1,
-	PSAB_PAD_UP     = 2,
-	PSAB_PAD_DOWN   = 3,
-	PSAB_TRIANGLE   = 4,
-	PSAB_CIRCLE     = 5,
-	PSAB_CROSS      = 6,
-	PSAB_SQUARE     = 7,
-	PSAB_L1         = 8,
-	PSAB_R1         = 9,
-	PSAB_L2         = 10,
-	PSAB_R2         = 11
-};
-
-/** \brief Number of digital buttons
- *
- * Includes \a everything, i.e.: 4 directions, Square, Cross, Circle, Triangle,
- * L1/2/3, R1/2/3, Select and Start.
- *
- * This is the number of entries in #PsxButton.
- */
-const byte PSX_BUTTONS_NO = 16;
-
-/** \brief Type that is used to report button presses
- */
-typedef uint16_t PsxButtons;
-
-/** \brief Size of buffer holding analog button data
- *
- * This is the size of the array returned by getAnalogButtonData().
- */
-const byte PSX_ANALOG_BTN_DATA_SIZE = 12;
-
-//! \name Controller Commands
-//! @{
-/** \brief Enter Configuration Mode
- * 
- * Command used to enter the controller configuration (also known as \a escape)
- * mode
- */
-static const byte enter_config[] = {0x01, 0x43, 0x00, 0x01, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
-static const byte exit_config[] = {0x01, 0x43, 0x00, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
-/* These shorter versions of enter_ and exit_config are accepted by all
- * controllers I've tested, even in analog mode, EXCEPT SCPH-1200, so let's use
- * the longer ones
- */
-//~ static byte enter_config[] = {0x01, 0x43, 0x00, 0x01, 0x00};
-//~ static byte exit_config[] = {0x01, 0x43, 0x00, 0x00, 0x00};
-
-/** \brief Read Controller Type
- * 
- * Command used to read the controller type.
- * 
- * This does not seem to be 100% reliable, or at least we don't know how to tell
- * all the various controllers apart.
- */
-static const byte type_read[] = {0x01, 0x45, 0x00, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A, 0x5A};
-static const byte set_mode[] = {0x01, 0x44, 0x00, /* enabled */ 0x01, /* locked */ 0x03, 0x00, 0x00, 0x00, 0x00};
-static const byte set_pressures[] = {0x01, 0x4F, 0x00, 0xFF, 0xFF, 0x03, 0x00, 0x00, 0x00};
-//~ static byte enable_rumble[] = {0x01, 0x4D, 0x00, 0x00, 0x01};
-
-/** \brief Poll all buttons
- * 
- * Command used to read the status of all buttons.
- */
-static const byte poll[] = {0x01, 0x42, 0x00, 0xFF, 0xFF};
-//! @}
-
-/** \brief Controller Type
- *
- * This is somehow derived from the reply to the #type_read command. It is NOT
- * much trustworthy, so it might be removed in the future.
- *
- * \sa getControllerType
- */
-enum PsxControllerType {
-	PSCTRL_UNKNOWN = 0,			//!< No idea
-	PSCTRL_DUALSHOCK,			//!< DualShock or compatible
-	PSCTRL_DSWIRELESS,			//!< Sony DualShock Wireless
-	PSCTRL_GUITHERO,			//!< Guitar Hero controller
-};
-
-/** \brief Number of different controller types recognized
- *
- * This is the number of entries in #PsxControllerType.
- */
-const byte PSCTRL_MAX = static_cast<byte> (PSCTRL_GUITHERO) + 1;
-
-
-/** \brief Controller Protocol
- *
- * Identifies the protocol the controller uses to report axes positions and
- * button presses. It's quite more reliable than #PsxControllerType, so use this
- * if you must.
- *
- * \sa getProtocol
- */
-enum PsxControllerProtocol {
-	PSPROTO_UNKNOWN = 0,		//!< No idea
-	PSPROTO_DIGITAL,			//!< Original controller (SCPH-1010) protocol (8 digital buttons + START + SELECT)
-	PSPROTO_DUALSHOCK,			//!< DualShock (has analog axes)
-	PSPROTO_DUALSHOCK2,			//!< DualShock 2 (has analog axes and buttons)
-	PSPROTO_FLIGHTSTICK,		//!< Green-mode (like DualShock but missing SELECT, L3 and R3)
-	PSPROTO_NEGCON,				//!< Namco neGcon (has 1 analog X axis and analog Square, Circle and L1 buttons)
-	PSPROTO_JOGCON				//!< Namco Jogcon (Wheel is mapped to analog X axis, half a rotation in each direction)
-};
-
-/** \brief Number of different protocols supported
- *
- * This is the number of entries in #PsxControllerProtocol.
- */
-const byte PSPROTO_MAX = static_cast<byte> (PSPROTO_JOGCON) + 1;
-
-/** \brief Analog sticks minimum value
- * 
- * Minimum value reported by analog sticks. This usually means that the stick is
- * fully either at the top or left position. Note that some sticks might not get
- * fully down to this value.
- *
- * \sa ANALOG_MAX_VALUE
- * \sa ANALOG_IDLE_VALUE
- */
-const byte ANALOG_MIN_VALUE = 0U;
-
-/** \brief Analog sticks maximum value
- * 
- * Maximum value reported by analog sticks. This usually means that the stick is
- * fully either at the bottom or right position. Note that some sticks might not
- * get fully up to this value.
- *
- * \sa ANALOGI_MAX_VALUE
- * \sa ANALOG_IDLE_VALUE
- */
-const byte ANALOG_MAX_VALUE = 255U;
-
-/** \brief Analog sticks idle value
- * 
- * Value reported when an analog stick is in the (ideal) center position. Note
- * that old and worn-out sticks might not self-center perfectly when released,
- * so you should never rely on this precise value to be reported.
- *
- * Also note that the up/down and left/right ranges are off by one, since values
- * 0-127 represent up/left and 129-255 mean down/right. The former interval
- * contains 128 different values, while the latter only 127. Sometimes you will
- * need to take this in consideration.
- */
-const byte ANALOG_IDLE_VALUE = 128U;
-
-/** \brief neGcon I/II-button press threshold
- *
- * The neGcon does not report digital button press data for its analog buttons,
- * so we have to make it up. The Square, Cross digital buttons will be
- * reported as pressed when the analog value of the II and I buttons
- * (respectively), goes over this threshold.
- *
- * \sa NEGCON_L_BUTTON_THRESHOLD
- */
-const byte NEGCON_I_II_BUTTON_THRESHOLD = 128U;
-
-/** \brief neGcon L-button press threshold
- *
- * The neGcon does not report digital button press data for its analog buttons,
- * so we have to make it up. The L1 digital button will be reported as pressed
- * when the analog value of the L buttons goes over this threshold.
- *
- * This value has been tuned so that the L button gets digitally triggered at
- * about the same point as the non-analog R button. This is done "empirically"
- * and might need tuning on a different controller than the one I actually have.
- * 
- * \sa NEGCON_I_II_BUTTON_THRESHOLD
- */
-const byte NEGCON_L_BUTTON_THRESHOLD = 240U;
-
 #include "PsxDriver.h"
+#include "PsxPublicTypes.h"
+#include "PsxOptions.h"
+#include "PsxCommands.h"
 
 
 /** \brief PSX Controller Interface
@@ -268,50 +43,9 @@ const byte NEGCON_L_BUTTON_THRESHOLD = 240U;
 class PsxController {
 protected:
 	PsxDriver *driver;
+
+	PsxSingleController controller;
 	
-	/** \brief Previous (Digital) Button Status
-	 * 
-	 * The individual bits can be identified through #PsxButton.
-	 */
-	PsxButtons previousButtonWord;
-
-	/** \brief (Digital) Button Status
-	 * 
-	 * The individual bits can be identified through #PsxButton.
-	 */
-	PsxButtons buttonWord;
-
-	/** \brief Controller Protocol
-	 *
-	 * The protocol controller data was interpreted with at the last call to
-	 * read()
-	 *
-	 * \sa getProtocol
-	 */
-	PsxControllerProtocol protocol;
-
-	//! \name Analog Stick Data
-	//! @{
-	byte lx;		//!< Horizontal axis of left stick [0-255, L to R]
-	byte ly;		//!< Vertical axis of left stick [0-255, U to D]
-	byte rx;		//!< Horizontal axis of right stick [0-255, L to R]
-	byte ry;		//!< Vertical axis of right stick [0-255, U to D]
-	
-	boolean analogSticksValid;	//!< True if the above were valid at the last call to read()
-	//! @}
-	
-	/** \brief Analog Button Data
-	 * 
-	 * \todo What's the meaning of every individual byte?
-	 */
-	byte analogButtonData[PSX_ANALOG_BTN_DATA_SIZE];
-
-	/** \brief Analog Button Data Validity
-	 * 
-	 * True if the #analogButtonData were valid in last call to read()
-	 */
-	boolean analogButtonDataValid;
-
 	// Green Mode controllers
 	inline boolean isFlightstickReply (const byte *status) {
 		return (status[1] & 0xF0) == 0x50;
@@ -356,18 +90,9 @@ public:
 	 */
 	virtual boolean begin (PsxDriver& drv) {
 		driver = &drv;
+
+		controller.clear ();
 		
-		// Start with all analog axes at midway position
-		lx = ANALOG_IDLE_VALUE;		
-		ly = ANALOG_IDLE_VALUE;
-		rx = ANALOG_IDLE_VALUE;
-		ry = ANALOG_IDLE_VALUE;
-
-		analogSticksValid = false;
-		memset (analogButtonData, 0, sizeof (analogButtonData));
-
-		protocol = PSPROTO_UNKNOWN;
-
 		// Some disposable readings to let the controller know we are here
 		for (byte i = 0; i < 5; ++i) {
 			read ();
@@ -591,7 +316,7 @@ public:
 	 * \return The controller protocol
 	 */
 	PsxControllerProtocol getProtocol () const {
-		return protocol;
+		return controller.protocol;
 	}
 
 	/** \brief Poll the controller
@@ -615,8 +340,8 @@ public:
 	boolean read () {
 		boolean ret = false;
 
-		analogSticksValid = false;
-		analogButtonDataValid = false;
+		controller.analogSticksValid = false;
+		controller.analogButtonDataValid = false;
 
 		driver -> attention ();
 		byte *in = driver -> autoShift (poll, 3);
@@ -628,30 +353,30 @@ public:
 				exitConfigMode ();
 			} else {
 				// We surely have buttons
-				previousButtonWord = buttonWord;
-				buttonWord = ((PsxButtons) in[4] << 8) | in[3];
+				controller.previousButtonWord = controller.buttonWord;
+				controller.buttonWord = ((PsxButtons) in[4] << 8) | in[3];
 
 				// See if we have anything more to read
 				if (isDualShock2Reply (in)) {
-					protocol = PSPROTO_DUALSHOCK2;
+					controller.protocol = PSPROTO_DUALSHOCK2;
 				} else if (isDualShockReply (in)) {
-					protocol = PSPROTO_DUALSHOCK;
+					controller.protocol = PSPROTO_DUALSHOCK;
 				} else if (isFlightstickReply (in)) {
-					protocol = PSPROTO_FLIGHTSTICK;
+					controller.protocol = PSPROTO_FLIGHTSTICK;
 				} else if (isNegconReply (in)) {
-					protocol = PSPROTO_NEGCON;
+					controller.protocol = PSPROTO_NEGCON;
 				} else if (isJogconReply (in)) {
-					protocol = PSPROTO_JOGCON;
+					controller.protocol = PSPROTO_JOGCON;
 				} else {
-					protocol = PSPROTO_DIGITAL;
+					controller.protocol = PSPROTO_DIGITAL;
 				}
 
-				switch (protocol) {
+				switch (controller.protocol) {
 					case PSPROTO_DUALSHOCK2:
 						// We also have analog button data
-						analogButtonDataValid = true;
+						controller.analogButtonDataValid = true;
 						for (int i = 0; i < PSX_ANALOG_BTN_DATA_SIZE; ++i) {
-							analogButtonData[i] = in[i + 9];
+							controller.analogButtonData[i] = in[i + 9];
 						}
 						/* Now fall through to DualShock case, the next line
 						 * avoids GCC warning
@@ -660,32 +385,32 @@ public:
 					case PSPROTO_DUALSHOCK:
 					case PSPROTO_FLIGHTSTICK:
 						// We have analog stick data
-						analogSticksValid = true;
-						rx = in[5];
-						ry = in[6];
-						lx = in[7];
-						ly = in[8];
+						controller.analogSticksValid = true;
+						controller.rx = in[5];
+						controller.ry = in[6];
+						controller.lx = in[7];
+						controller.ly = in[8];
 						break;
 					case PSPROTO_NEGCON:
 						// Map the twist axis to X axis of left analog
-						analogSticksValid = true;
-						lx = in[5];
+						controller.analogSticksValid = true;
+						controller.lx = in[5];
 
 						// Map analog button data to their reasonable counterparts
-						analogButtonDataValid = true;
-						analogButtonData[PSAB_CROSS] = in[6];
-						analogButtonData[PSAB_SQUARE] = in[7];
-						analogButtonData[PSAB_L1] = in[8];
+						controller.analogButtonDataValid = true;
+						controller.analogButtonData[PSAB_CROSS] = in[6];
+						controller.analogButtonData[PSAB_SQUARE] = in[7];
+						controller.analogButtonData[PSAB_L1] = in[8];
 
 						// Make up "missing" digital data
-						if (analogButtonData[PSAB_SQUARE] >= NEGCON_I_II_BUTTON_THRESHOLD) {
-							buttonWord &= ~PSB_SQUARE;
+						if (controller.analogButtonData[PSAB_SQUARE] >= NEGCON_I_II_BUTTON_THRESHOLD) {
+							controller.buttonWord &= ~PSB_SQUARE;
 						}
-						if (analogButtonData[PSAB_CROSS] >= NEGCON_I_II_BUTTON_THRESHOLD) {
-							buttonWord &= ~PSB_CROSS;
+						if (controller.analogButtonData[PSAB_CROSS] >= NEGCON_I_II_BUTTON_THRESHOLD) {
+							controller.buttonWord &= ~PSB_CROSS;
 						}
-						if (analogButtonData[PSAB_L1] >= NEGCON_L_BUTTON_THRESHOLD) {
-							buttonWord &= ~PSB_L1;
+						if (controller.analogButtonData[PSAB_L1] >= NEGCON_L_BUTTON_THRESHOLD) {
+							controller.buttonWord &= ~PSB_L1;
 						}
 						break;
 					case PSPROTO_JOGCON:
@@ -702,17 +427,17 @@ public:
 						 * We'll want to cap the movement halfway in each
 						 * direction, for ease of use/implementation.
 						 */
-						analogSticksValid = true;
+						controller.analogSticksValid = true;
 						if (in[6] < 0x80) {
 							// CW up to half
-							lx = in[5] < 0x80 ? in[5] : (0x80 - 1);
+							controller.lx = in[5] < 0x80 ? in[5] : (0x80 - 1);
 						} else {
 							// CCW down to half
-							lx = in[5] > 0x80 ? in[5] : (0x80 + 1);
+							controller.lx = in[5] > 0x80 ? in[5] : (0x80 + 1);
 						}
 
 						// Bring to the usual 0-255 range
-						lx += 0x80;
+						controller.lx += 0x80;
 						break;
 					default:
 						// We are already done
@@ -732,7 +457,7 @@ public:
 	 *         call to read(), false otherwise
 	 */
 	boolean buttonsChanged () const {
-		return ((previousButtonWord ^ buttonWord) > 0);
+		return ((controller.previousButtonWord ^ controller.buttonWord) > 0);
 	}
 
 	/** \brief Check if a button has changed state
@@ -741,7 +466,7 @@ public:
 	 *         call to read(), false otherwise
 	 */
 	boolean buttonChanged (const PsxButtons button) const {
-		return (((previousButtonWord ^ buttonWord) & button) > 0);
+		return (((controller.previousButtonWord ^ controller.buttonWord) & button) > 0);
 	}
 
 	/** \brief Check if a button is currently pressed
@@ -751,7 +476,7 @@ public:
 	 *         otherwise
 	 */
 	boolean buttonPressed (const PsxButton button) const {
-		return buttonPressed (~buttonWord, button);
+		return buttonPressed (~controller.buttonWord, button);
 	}
 
 	/** \brief Check if a button is pressed in a Button Word
@@ -781,7 +506,7 @@ public:
 	 *         is not now, false otherwise
 	 */
 	boolean buttonJustReleased (const PsxButton button) const {
-		return (buttonChanged (button) & ((~previousButtonWord & button) > 0));
+		return (buttonChanged (button) & ((~controller.previousButtonWord & button) > 0));
 	}
 
 	/** \brief Check if NO button is pressed in a Button Word
@@ -799,7 +524,7 @@ public:
 	 *         false otherwise
 	 */
 	boolean noButtonPressed (void) const {
-		return buttonWord == ~PSB_NONE;
+		return controller.buttonWord == ~PSB_NONE;
 	}
 	
 	/** \brief Retrieve the <em>Button Word</em>
@@ -813,7 +538,7 @@ public:
 	 * \return the Button Word
 	 */
 	PsxButtons getButtonWord () const {
-		return ~buttonWord;
+		return ~controller.buttonWord;
 	}
 
 	/** \brief Retrieve button pressure depth/strength
@@ -832,8 +557,8 @@ public:
 	byte getAnalogButton (const PsxAnalogButton button) const {
 		byte ret = 0;
 		
-		if (analogButtonDataValid) {
-			ret = analogButtonData[button];
+		if (controller.analogButtonDataValid) {
+			ret = controller.analogButtonData[button];
 		//~ } else if (buttonPressed (button)) {		// FIXME
 			//~ // No analog data, assume fully pressed or fully released
 			//~ ret = 0xFF;
@@ -845,7 +570,7 @@ public:
 	/** \brief Retrieve all analog button data
 	 */
 	const byte* getAnalogButtonData () const {
-		return analogButtonDataValid ? analogButtonData : NULL;
+		return controller.analogButtonDataValid ? controller.analogButtonData : NULL;
 	}
 
 	/** \brief Retrieve position of the \a left analog stick
@@ -862,10 +587,10 @@ public:
 	 * \return true if the returned position is valid, false otherwise
 	 */
 	boolean getLeftAnalog (byte& x, byte& y) const {
-		x = lx;
-		y = ly;
+		x = controller.lx;
+		y = controller.ly;
 
-		return analogSticksValid;
+		return controller.analogSticksValid;
 	}
 
 	/** \brief Retrieve position of the \a right analog stick
@@ -883,10 +608,10 @@ public:
 	 * \return true if the returned position is valid, false otherwise
 	 */
 	boolean getRightAnalog (byte& x, byte& y) {
-		x = rx;
-		y = ry;
+		x = controller.rx;
+		y = controller.ry;
 
-		return analogSticksValid;
+		return controller.analogSticksValid;
 	}
 	
 	//! @}		// Polling Functions
