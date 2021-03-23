2145f52bb22c3173d4dbe995ed0d54368ae3a589
SukkoPera <software@sukkology.net>
2021-03-23 08:41:17 +0100

    Add header

--- src/PsxOptions.h
+++ src/PsxOptions.h
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of PsxNewLib.                                             *
+ *                                                                             *
+ * Copyright (C) 2019-2020 by SukkoPera <software@sukkology.net>               *
+ *                                                                             *
+ * PsxNewLib is free software: you can redistribute it and/or                  *
+ * modify it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * PsxNewLib is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with PsxNewLib. If not, see http://www.gnu.org/licenses.              *
+ ******************************************************************************/
+/**
+ * \file PsxMultiTap.h
+ * \author SukkoPera <software@sukkology.net>
+ * \date 22 Mar 2021
+ * \brief Playstation Controller Misc Options
+ * 
+ * Please refer to the GitHub page and wiki for any information:
+ * https://github.com/SukkoPera/PsxNewLib
+ */
+
 #include <Arduino.h>
 
 /** \brief Command timeout (ms)
